6908e7700b8fc88483c47e733254c63e74aaf8d9
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2021-05-19 09:34:31 -0300

    add apps

---
+++ flutter-crashlytics-master/lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:alura_crashlytics/screens/dashboard.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  } else {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    FirebaseCrashlytics.instance.setUserIdentifier('Danilo Carlos');
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  }
+  runZonedGuarded<Future<void>>(() async {
+    runApp(BytebankApp());
+  }, FirebaseCrashlytics.instance.recordError);
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Color.fromRGBO(71, 161, 56, 1),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Color.fromRGBO(71, 161, 56, 1),
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
